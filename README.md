516f616a3f08086e8b272a5580c0737eacae2666
Sakherr <sakhershteyat@gmail.com>
2023-06-14 11:50:54 +0000

    second

---
+++ README.md
@@ -0,0 +1,12 @@
+# LAB - Class 17
+## Project: Web Scraping
+## Author: Sakher shteyat 
+
+
+
+# How to initialize/run your application (where applicable)
+
+ ### python scarper.py
+
+# TESTS
+## No test needed just run the code "python scarper.py"
--- scraper.py
+++ scraper.py
@@ -1,36 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_citations_needed_count(url):
+    """
+    Scrape a Wikipedia page and return the number of citations needed.
+
+    Args:
+        url: The URL of the Wikipedia page.
+
+    Returns:
+        The number of citations needed as an integer.
+    """
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    citations_needed = []
-    for p in soup.find_all("p"):
-        if p.find("b", text="Citation needed"):
-            citations_needed.append(p)
-
+    soup = BeautifulSoup(response.content, 'html.parser')
+    citations_needed = soup.find_all(text="citation needed")
     return len(citations_needed)
 
+
 def get_citations_needed_report(url):
+    """
+    Scrape a Wikipedia page and generate a report of passages needing citations.
+
+    Args:
+        url: The URL of the Wikipedia page.
+
+    Returns:
+        A string containing the report with each citation listed in the order found.
+    """
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    citations_needed = []
-    for p in soup.find_all("p"):
-        if p.find("b", text="Citation needed"):
-            citations_needed.append(p)
-
+    soup = BeautifulSoup(response.content, 'html.parser')
+    citations_needed = soup.find_all(text="citation needed")
     report = ""
     for citation in citations_needed:
-        report += citation.text + "\n"
-
+        passage = citation.find_parent('p').text
+        report += f"Citation needed for: \"{passage}\"\n\n"
     return report
 
-if __name__ == "__main__":
-    url = "https://en.wikipedia.org/wiki/History_of_Mexico"
-    count = get_citations_needed_count(url)
-    report = get_citations_needed_report(url)
 
-    print("Number of citations needed:", count)
-    print("Report:", report)
+# Example usage
+url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+
+citation_count = get_citations_needed_count(url)
+print(f"Number of citations needed: {citation_count}\n")
+
+citation_report = get_citations_needed_report(url)
+print("Citations needed:\n")
+print(citation_report)
